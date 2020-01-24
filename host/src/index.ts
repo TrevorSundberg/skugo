b5b587014ec008fafb0b0bfa9e3d613778e95d89
Trevor Sundberg <tsundberg@s2.systems>
2020-01-24 09:24:36 -0800

    Added basic host script

---
+++ host/src/index.ts
@@ -0,0 +1,7 @@
+/*
+ * Generate a url (uuid4)
+ * Connect to the websocket so we can proxy traffic
+ * Parse command line parameters (such as --password or -p)
+ * The 'command' then sets what we choose (ssh, vnc, folder, etc)
+ */
+console.log("Hello host!");
