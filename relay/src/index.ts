d9142abdf9af8b5883e619d293998f8a4d48a088
Trevor Sundberg <tsundberg@s2.systems>
2020-01-24 01:33:58 -0800

    Made the websocket relay server

--- relay/src/index.ts
+++ relay/src/index.ts
@@ -1,5 +1,36 @@
 import WebSocket from "ws";
 
-new WebSocket.Server({
+const wss = new WebSocket.Server({
   port: 80
+});
+
+const pairs: Record<string, WebSocket[]> = {};
+
+wss.on("connection", (socket, request) => {
+  const {url} = request;
+  const sockets = pairs[url] || [];
+  sockets.push(socket);
+  pairs[url] = sockets;
+
+  socket.on("message", (data) => {
+    for (const otherSocket of sockets) {
+      if (otherSocket !== socket) {
+        if (otherSocket.readyState === WebSocket.OPEN) {
+          otherSocket.send(data);
+        }
+      }
+    }
+  });
+
+  socket.on("close", () => {
+    socket.close();
+    const index = sockets.indexOf(socket);
+    if (index !== -1) {
+      sockets.splice(index, 1);
+    }
+
+    if (sockets.length === 0) {
+      delete pairs[url];
+    }
+  });
 });
