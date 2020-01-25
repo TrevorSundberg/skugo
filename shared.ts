9194df5bb6b563562f3f8e1837977501ba2dfbb3
Trevor Sundberg <tsundberg@s2.systems>
2020-01-25 11:22:54 -0800

    Added shared urls file

---
+++ shared.ts
@@ -0,0 +1,9 @@
+export const isDev = typeof location === "undefined" ? process.env.NODE_ENV === "dev" : location.protocol === "http:";
+
+if (isDev) {
+  console.log("Development Environment");
+}
+
+export const getWebSocketUrl = () => isDev ? "ws://localhost:80/" : "wss://skugo.openode.io/";
+
+export const getPageUrl = () => isDev ? "http://localhost:8080/" : "https://trevorsundberg.github.io/skugo/";
